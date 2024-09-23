05b6c55aeb60f331824b0ff5d08a2a6810650631
Agshinhummatov <agshinshh@code.edu.az>
2024-09-23 15:56:34 +0400

    feat: Add DiscountService with Dapper support
    - Implemented CRUD methods for coupons
    - Used Dapper for database operations

---
+++ MultiShop/Discount/MultiShop.Discount/Services/DiscountService.cs
@@ -0,0 +1,85 @@
+﻿using Dapper;
+using MultiShop.Discount.Context;
+using MultiShop.Discount.DTOs;
+
+namespace MultiShop.Discount.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        private readonly DapperContext _dapperContext;
+
+        public DiscountService(DapperContext dapperContext)
+        {
+            _dapperContext = dapperContext;
+        }
+
+        public async Task CreateCouponAsync(CreateCouponDto createCouponDto)
+        {
+            string query = "insert into Coupons (Code,Rate,IsActive,ValidDate) values(@code,@rate,@isActive,@validDate)";
+            var parametrs = new DynamicParameters();
+            parametrs.Add("@code", createCouponDto.Code);
+            parametrs.Add("@rate", createCouponDto.Rate);
+            parametrs.Add("@isActive", createCouponDto.IsActive);
+            parametrs.Add("@validDate", createCouponDto.ValidDate);
+            using(var connection = _dapperContext.CreateConnection())
+            {
+              await connection.ExecuteAsync(query, parametrs);
+            }
+
+        }
+
+        public async Task DeleteCouponAsync(int id)
+        {
+            string query = "Delete From Coupons where CouponId=@couponId";
+            var parametrs = new DynamicParameters();
+            parametrs.Add("couponId", id);
+
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parametrs);
+            }
+        }
+
+
+
+        public async Task<List<ResultCouponDto>> GetAllCouponAsync()
+        {
+            string query = "Select * From Coupons";
+
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultCouponDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<GetByIdCouponDto> GetByIdCouponAsync(int id)
+        {
+            string query = "Select * From Coupons Where CouponId=@couponId";
+
+            var paramerts = new DynamicParameters();
+
+            paramerts.Add("couponId", id);
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                return  await connection.QueryFirstOrDefaultAsync<GetByIdCouponDto>(query);
+               
+            }
+        }
+
+        public async Task UpdateCouponAsync(UpdateCouponDto updateCouponDto)
+        {
+            string query = "Update Coupons Set Code=@code,Rate=@rate,IsActive=@isActive,ValidDate=@validDate where CouponId=@couponId";
+
+            var parametrs = new DynamicParameters();
+            parametrs.Add("@code", updateCouponDto.Code);
+            parametrs.Add("@rate", updateCouponDto.Rate);
+            parametrs.Add("@isActive", updateCouponDto.IsActive);
+            parametrs.Add("@validDate", updateCouponDto.ValidDate);
+            using (var connection = _dapperContext.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parametrs);
+            }
+        }
+    }
+}
--- MultiShop/Discount/MultiShop.Discount/obj/Debug/net6.0/MultiShop.Discount.AssemblyInfo.cs
+++ MultiShop/Discount/MultiShop.Discount/obj/Debug/net6.0/MultiShop.Discount.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2632fb63a423346b7c26fbfb88ea5682ad29018")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41834b7a7730815c3fcadeb736a501a4979987ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Discount")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
