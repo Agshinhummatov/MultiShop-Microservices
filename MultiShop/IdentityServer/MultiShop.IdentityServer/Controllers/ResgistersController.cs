7649795703d6df5dbf06d5ee7cfa93686c5e2e40
Agshinhummatov <agshinshh@code.edu.az>
2024-10-11 15:02:19 +0400

    Add IdentityServer4 and ASP.NET Core Identity for user authentication and authorization

---
+++ MultiShop/IdentityServer/MultiShop.IdentityServer/Controllers/ResgistersController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.DTOs;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResgistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        
+        public ResgistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); 
+            }
+
+            var user = new ApplicationUser()
+            {
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+            };
+
+            var result = await _userManager.CreateAsync(user, userRegisterDto.Password);
+
+            if (result.Succeeded)
+            {
+                // Optionally, you can send a confirmation email here
+                return Ok("Registration successful");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return BadRequest(ModelState);
+
+            }
+        }
+    }
+}
--- MultiShop/IdentityServer/MultiShop.IdentityServer/obj/Debug/netcoreapp3.1/MultiShop.IdentityServer.AssemblyInfo.cs
+++ MultiShop/IdentityServer/MultiShop.IdentityServer/obj/Debug/netcoreapp3.1/MultiShop.IdentityServer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7747afe22306e9362de338a242310718edda3811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a423a7b21204a50cf06974510d6e5853f731cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
